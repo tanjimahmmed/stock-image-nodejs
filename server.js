29ba887b4c783555c09534a4c7e78d659411110b
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-04-15 13:09:52 +0600

    add route for render

--- server.js
+++ server.js
@@ -30,3 +30,7 @@ app.get("/items", async (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
 });
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"))
+})
